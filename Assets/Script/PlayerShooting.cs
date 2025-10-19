e85f14616bc4ce4cbefe3ca18a144ef262ae4051
Kangyelim1 <gyelim887@gmail.com>
2025-10-19 21:56:38 +0900

    무기 집 추가

--- Assets/Script/PlayerShooting.cs
+++ Assets/Script/PlayerShooting.cs
@@ -12,7 +12,11 @@ public class PlayerShooting : MonoBehaviour
 
     int currentWeapon = 0;
 
+    // 근접 무기 변수 추가
+    [SerializeField]
+    private GameObject meleeWeaponObject;
 
+    private float swingDuration = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -54,13 +58,34 @@ public class PlayerShooting : MonoBehaviour
         }
         else
         {
+            if (meleeWeaponObject != null)
+            {
+                // 근접 무기 로직
+                meleeWeaponObject.SetActive(true);
+                StartCoroutine(SwingWeaponForDuration(swingDuration));
+            }
+            else
+            {
+                Debug.LogError("Melee Weapon Object가 Inspector에 할당되지 않았습니다!");
+            }
             // ... 기존 코드
-            GameObject proj = Instantiate(projectilePrefab2, firePoint.position, Quaternion.identity);
+            // GameObject proj = Instantiate(projectilePrefab2, firePoint.position, Quaternion.identity);
 
             // Projectile 스크립트 가져오기
-            Projectile projectileScript = proj.GetComponent<Projectile>();
-            projectileScript.speed = 20f; // 두 번째 무기 속도
+            //  Projectile projectileScript = proj.GetComponent<Projectile>();
+            // projectileScript.speed = 20f; // 두 번째 무기 속도
         }
 
+    }
+    IEnumerator SwingWeaponForDuration(float duration)
+    {
+        // 설정된 시간(duration)만큼 대기합니다.
+        yield return new WaitForSeconds(duration);
+
+        // 대기가 끝나면 무기 오브젝트를 다시 비활성화하여 숨깁니다.
+        if (meleeWeaponObject != null)
+        {
+            meleeWeaponObject.SetActive(false);
+        }
     }
 }
